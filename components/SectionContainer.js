566c93f21681b5cffd16aa4566e8e22fb848509b
Saugat Adhikari <asaugat04@gmail.com>
2024-04-09 23:33:54 +0545

    Refactor code and remove unused files

--- components/SectionContainer.js
+++
@@ -1,5 +0,0 @@
-export default function SectionContainer({ children }) {
-  return (
-    <div className="mx-auto my-auto max-w-2xl px-4 sm:px-6 xl:max-w-2xl xl:px-0">{children}</div>
-  );
-}
--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'home' },
-  { href: '/blog', title: 'notes' },
-  { href: '/projects', title: 'projects' },
-  { href: '/about', title: 'about' },
-];
-
-export default headerNavLinks;
